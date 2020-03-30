cbf554943fd3060323382a970d37bf5c422f01b2
aof2 <aof2.2016@gmail.com>
2020-03-30 17:34:11 +0700

    ! Added new Unity project with gitignore

---
+++ Unity/Assets/CubeController2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController2 : MonoBehaviour
+{
+    public GameObject cubecontroller2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeColor(Color a)
+    {
+        this.transform.GetComponent<MeshRenderer>().material.color = a;
+    }
+}
