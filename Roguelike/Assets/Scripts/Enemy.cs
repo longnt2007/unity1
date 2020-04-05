c2cb1126cff18b209c8e8dee8ba26204e5f47aaf
aof2 <aof2.2016@gmail.com>
2020-04-05 18:12:46 +0700

    - Step 13 / 14

--- Roguelike/Assets/Scripts/Enemy.cs
+++ Roguelike/Assets/Scripts/Enemy.cs
@@ -10,6 +10,9 @@ public class Enemy : MovingObject
 
     private bool skipMove;
 
+    public AudioClip enemyAttack1;
+    public AudioClip enemyAttack2;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -49,6 +52,7 @@ public class Enemy : MovingObject
     {
         Player hitPlayer = component as Player;
         animator.SetTrigger("enemyAttack");
+        SoundManager.instance.RandomizeSfx(enemyAttack1,enemyAttack2);
         hitPlayer.LoseFood(playerDamage);
     }
 }
---
+++ Roguelike/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSouce;
+    public AudioSource musicSouce;
+    public static SoundManager instance = null;
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle (AudioClip clip)
+    {
+        efxSouce.clip = clip;
+        efxSouce.Play();
+    }
+
+    public void RandomizeSfx( params AudioClip [] clips)
+    {
+        int randomIndex = Random.Range (0, clips.Length);
+        float randomPitch = Random.Range (lowPitchRange, highPitchRange);
+
+        efxSouce.pitch = randomPitch;
+        efxSouce.clip = clips[randomIndex];
+        efxSouce.Play();
+    }
+}
